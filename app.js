630d9dac05f0122f88e9d2e01d28732fe36356ed
David <david.shams@gmail.com>
2019-08-14 16:29:40 -0500

    Express listening Request object logged

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    
+    console.log(req);
+    res.send('Express!');
+})
+
+let port = 9090;
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
 })
