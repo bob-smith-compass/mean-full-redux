f5572ff665dd461432d1cec93080bd3359adf0aa
David <david.shams@gmail.com>
2019-08-14 16:35:56 -0500

    chalk colors

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var express = require('express');
+var chalk = require('chalk');
 
 var app = express();
 
 app.get('/', (req, res) => {
-    console.log(req);
+    // console.log(req);
+    console.log(req.headers);
     res.send('Express!');
 })
 
 let port = 9090;
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`);
+    console.log(`listening on port ${chalk.green(port)}`);
 })
